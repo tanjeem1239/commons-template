c4ac3ca9c333dd56ecf53b51bb706806af143cc3
tanjeem-bs23 <tanjeem.alam@brainstation-23.com>
2023-05-26 16:42:58 +0600

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function() {
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 0,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
