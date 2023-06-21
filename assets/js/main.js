55d59d647e7b9fd03e1e5438d61a6d7b3a990b37
Arman-Hossain-77 <arman.hossain@brainstation-23.com>
2023-06-21 19:45:00 +0600

    change js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,6 @@ import("https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.mi
 import("https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js");
 
 
-(function() {
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 0,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
-
-})()
-
-
 // Nav Menu
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -40,59 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Slider
 
-document.addEventListener('DOMContentLoaded', function() {
-  var productWidth = document.querySelector('.product').offsetWidth;
-  var containerWidth = document.querySelector('.slider-container').offsetWidth;
-  var containerScrollWidth = document.querySelector('.slider-container').scrollWidth;
-  var scrollDistance = productWidth;
-
-  var leftBtn = document.getElementById('left-btn');
-  var rightBtn = document.getElementById('right-btn');
-  var sliderContainer = document.querySelector('.slider-container');
-  var storiesSection = document.querySelector('.explore-our-stories');
-
-  // Move the product div to the left
-  leftBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    var containerScrollLeft = sliderContainer.scrollLeft;
-    var targetScrollLeft = containerScrollLeft - scrollDistance;
-
-    if (targetScrollLeft < 0) {
-      targetScrollLeft = 0;
-    }
-
-    sliderContainer.scrollTo({
-      left: targetScrollLeft,
-      behavior: 'smooth'
-    });
-  });
-
-  // Move the product div to the right
-  rightBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    var containerScrollRight = sliderContainer.scrollRight;
-    var targetScrollRight = containerScrollRight + scrollDistance;
-    var targetScrollRight = containerScrollRight - containerDistance;
-
-    if (targetScrollRight > 0) {
-      targetScrollRight = 0;
-    }
-
-    sliderContainer.scrollTo({
-      right: targetScrollRight,
-      behavior: 'smooth'
-    });
-  });
-
-  // Prevent the "explore-our-stories" section from moving
-  storiesSection.addEventListener('click', function(event) {
-    event.stopPropagation();
-  });
-});
 
     // Swiper js
 
